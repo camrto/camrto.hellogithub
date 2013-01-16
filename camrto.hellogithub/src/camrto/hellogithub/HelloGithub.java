1e02a5ac4f5be50efbe654b3c73370bcfd85d7b3
Camrto Verievil <camrto@gmail.com>
2013-01-16 02:00:47 -0600

    starting project

---
+++ camrto.hellogithub/src/camrto/hellogithub/HelloGithub.java
@@ -0,0 +1,9 @@
+package camrto.hellogithub;
+
+public class HelloGithub {
+
+	public static void main(String[] args) {
+		System.out.println("hello github");
+		System.out.println("modified");
+	}
+}
