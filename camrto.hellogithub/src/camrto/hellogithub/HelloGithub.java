a90d54d38aa174078b9e5d7145e6c283936819a0
Camrto Verievil <camrto@gmail.com>
2013-01-16 02:27:50 -0600

    Revert "Revert "will revert""
    This reverts commit e28830cabd5ba75685d7b4e15d874fd98eacbc31.

--- camrto.hellogithub/src/camrto/hellogithub/HelloGithub.java
+++ camrto.hellogithub/src/camrto/hellogithub/HelloGithub.java
@@ -4,7 +4,7 @@ public class HelloGithub {
 
 	public static void main(String[] args) {
 		System.out.println("hello github");
-		System.out.println("more chanches");
+		System.out.println("will revert");
 		
 	}
 }
